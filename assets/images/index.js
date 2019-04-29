a1447af350cd45e9dca113e8ffa0ca561f4bb156
EugenePizzerbert <40814780+EugenePizzerbert@users.noreply.github.com>
2019-04-30 01:26:31 +0800

    Update index.js

--- assets/images/index.js
+++ assets/images/index.js
@@ -2,4 +2,118 @@ export { default as fox } from './5.jpg';
 export { default as cat } from './6.jpg';
 export { default as background } from './7.jpg';
 export { default as element } from './8.jpg';
-export { default as lion } from './9.jpg';
+export { default as lion } from './19.jpg';
+export { default as fox } from './15.jpg';
+export { default as cat } from './1.jpg';
+export { default as background } from './17.jpg';{
+    "dev": {
+        "awscloudformation": {
+            "AuthRoleName": "mobileapp-20190416153848-authRole",
+            "UnauthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-unauthRole",
+            "AuthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-authRole",
+            "Region": "eu-west-1",
+            "DeploymentBucketName": "mobileapp-20190416153848-deployment",
+            "UnauthRoleName": "mobileapp-20190416153848-unauthRole",
+            "StackName": "mobileapp-20190416153848",
+            "StackId": "arn:aws:cloudformation:eu-west-1:639564553884:stack/mobileapp-20190416153848/58cbaae0-6055-11e9-b206-065d51fe97c8"
+        },
+        "categories": {
+            "auth": {
+                "dailyjokecognito": {
+                    "facebookAppId": "417754792313820"
+                }
+            }
+        }
+    }
+}
+{
+    "dev": {
+        "awscloudformation": {
+            "AuthRoleName": "mobileapp-20190416153848-authRole",
+            "UnauthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-unauthRole",
+            "AuthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-authRole",
+            "Region": "eu-west-1",
+            "DeploymentBucketName": "mobileapp-20190416153848-deployment",
+            "UnauthRoleName": "mobileapp-20190416153848-unauthRole",
+            "StackName": "mobileapp-20190416153848",
+            "StackId": "arn:aws:cloudformation:eu-west-1:639564553884:stack/mobileapp-20190416153848/58cbaae0-6055-11e9-b206-065d51fe97c8"
+        },
+        "categories": {
+            "auth": {
+                "dailyjokecognito": {
+                    "facebookAppId": "417754792313820"
+                }
+            }
+        }
+    }
+}
+{
+    "dev": {
+        "awscloudformation": {
+            "AuthRoleName": "mobileapp-20190416153848-authRole",
+            "UnauthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-unauthRole",
+            "AuthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-authRole",
+            "Region": "eu-west-1",
+            "DeploymentBucketName": "mobileapp-20190416153848-deployment",
+            "UnauthRoleName": "mobileapp-20190416153848-unauthRole",
+            "StackName": "mobileapp-20190416153848",
+            "StackId": "arn:aws:cloudformation:eu-west-1:639564553884:stack/mobileapp-20190416153848/58cbaae0-6055-11e9-b206-065d51fe97c8"
+        },
+        "categories": {
+            "auth": {
+                "dailyjokecognito": {
+                    "facebookAppId": "417754792313820"
+                }
+            }
+        }
+    }
+}
+{
+    "dev": {
+        "awscloudformation": {
+            "AuthRoleName": "mobileapp-20190416153848-authRole",
+            "UnauthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-unauthRole",
+            "AuthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-authRole",
+            "Region": "eu-west-1",
+            "DeploymentBucketName": "mobileapp-20190416153848-deployment",
+            "UnauthRoleName": "mobileapp-20190416153848-unauthRole",
+            "StackName": "mobileapp-20190416153848",
+            "StackId": "arn:aws:cloudformation:eu-west-1:639564553884:stack/mobileapp-20190416153848/58cbaae0-6055-11e9-b206-065d51fe97c8"
+        },
+        "categories": {
+            "auth": {
+                "dailyjokecognito": {
+                    "facebookAppId": "417754792313820"
+                }
+            }
+        }
+    }
+}
+{
+    "dev": {
+        "awscloudformation": {
+            "AuthRoleName": "mobileapp-20190416153848-authRole",
+            "UnauthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-unauthRole",
+            "AuthRoleArn": "arn:aws:iam::639564553884:role/mobileapp-20190416153848-authRole",
+            "Region": "eu-west-1",
+            "DeploymentBucketName": "mobileapp-20190416153848-deployment",
+            "UnauthRoleName": "mobileapp-20190416153848-unauthRole",
+            "StackName": "mobileapp-20190416153848",
+            "StackId": "arn:aws:cloudformation:eu-west-1:639564553884:stack/mobileapp-20190416153848/58cbaae0-6055-11e9-b206-065d51fe97c8"
+        },
+        "categories": {
+            "auth": {
+                "dailyjokecognito": {
+                    "facebookAppId": "417754792313820"
+                }
+            }
+        }
+    }
+}
+-kunkka
+ 1. tango, shout sheld, iron branch, gauntles of strength.
+ 2. bottle, boot of speed, magic stick, bracer
+ 3. phase boots, magic wand, drum of edurance, shadow blade
+ 4. Daedalus, black king bar
+-
+ 
